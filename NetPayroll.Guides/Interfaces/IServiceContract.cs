7438f8fcb94b2db5f8faa3b3c5e256615f7e3d11
ladislavlisy <ladislav.lisy@seznam.cz>
2017-08-20 12:26:54 +0200

    Service Contract

--- NetPayroll.Guides/Interfaces/IServiceContract.cs
+++ NetPayroll.Guides/Interfaces/IServiceContract.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using NetPayroll.Guides.Common;
+
 namespace NetPayroll.Guides.Interfaces
 {
     public interface IServiceContract
     {
-    }
+        IProfileContract BuildProfile(Period period);
+	}
 }
